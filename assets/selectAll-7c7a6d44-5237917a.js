ab4758c1a3e09aa02ea11f1e9654e53de716dd0e
heng1025 <heng1025@users.noreply.github.com>
2023-07-16 16:02:54 +0000

    deploy: 945748506ca9fd54b625aaa6f38d220723c244da

---
+++ assets/selectAll-7c7a6d44-5237917a.js
@@ -0,0 +1,1 @@
+import{aS as t,aT as n,aU as o}from"./index-a137a4c2.js";function r(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{r as s};
