0ab1b3bdfaf5d68f798dcec4c8dedc43cbf8bcf4
heng1025 <heng1025@users.noreply.github.com>
2025-09-06 07:21:43 +0000

    deploy: 72f36904540b251847f77bfb64bd8677455dea88

---
+++ assets/md-CVWAFSCp.js
@@ -0,0 +1,16 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-C1lKNbSj.js";import{b as c,o as p,w as a,g as s,e as m,m as u,D as e,v as f,x as d,B as t}from"./modules/vue-BSBrl5ir.js";import{I as g}from"./slidev/default-D_1aDfUc.js";import{u as h,f as k}from"./slidev/context-CP-riexH.js";import"./modules/unplugin-icons-nYm-bVeF.js";import"./index-F_USY7Ft.js";import"./modules/shiki-CJEomhxH.js";const B={__name:"slides.md__slidev_16",setup(x){const{$clicksContext:l,$frontmatter:i}=h();return l.setup(),(v,n)=>{const o=r;return p(),c(g,f(d(t(k)(t(i),15))),{default:a(()=>[n[1]||(n[1]=s("h1",null,"Commit message",-1)),n[2]||(n[2]=s("p",null,[s("a",{href:"http://commitizen.github.io/cz-cli/",target:"_blank"},"工具(commitizen)")],-1)),m(o,u({},{title:"",ranges:[]}),{default:a(()=>[...n[0]||(n[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"$ git-cz")]),e(`
+`),s("span",{class:"line"},[s("span",null,"cz-cli@4.3.0, cz-conventional-changelog@3.3.0")]),e(`
+`),s("span",{class:"line"},[s("span")]),e(`
+`),s("span",{class:"line"},[s("span",null,"? Select the type of change that you're committing: (Use arrow keys)")]),e(`
+`),s("span",{class:"line"},[s("span",null,"❯ feat:     A new feature ")]),e(`
+`),s("span",{class:"line"},[s("span",null,"  fix:      A bug fix ")]),e(`
+`),s("span",{class:"line"},[s("span",null,"  docs:     Documentation only changes ")]),e(`
+`),s("span",{class:"line"},[s("span",null,"  style:    Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc) ")]),e(`
+`),s("span",{class:"line"},[s("span",null,"  refactor: A code change that neither fixes a bug nor adds a feature ")]),e(`
+`),s("span",{class:"line"},[s("span",null,"  perf:     A code change that improves performance ")]),e(`
+`),s("span",{class:"line"},[s("span",null,"  test:     Adding missing tests or correcting existing tests")]),e(`
+`),s("span",{class:"line"},[s("span",null,"  build:    Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm) ")]),e(`
+`),s("span",{class:"line"},[s("span",null,"  ci:       Changes to our CI configuration files and scripts (example scopes: Travis, Circle, SauceLabs) ")]),e(`
+`),s("span",{class:"line"},[s("span",null,"  chore:    Other changes that don't modify src or test files ")]),e(`
+`),s("span",{class:"line"},[s("span",null,"  revert:   Reverts a previous commit ")]),e(`
+`),s("span",{class:"line"},[s("span",null,"(Move up and down to reveal more choices)")])])],-1)])]),_:1},16)]),_:1},16)}}};export{B as default};
